2c6180fe4e1d1dcd31f4ca81aae4e535663b1505
j.yao <jackyao713911@gmail.com>
2018-08-31 00:49:42 +0800

    config security

--- src/main/java/com/jy/bootadmin/BootAdminApplication.java
+++ src/main/java/com/jy/bootadmin/BootAdminApplication.java
@@ -1,11 +1,15 @@
-package com.jy.bootadmin.bootadmin;
+package com.jy.bootadmin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableGlobalMethodSecurity
+@EnableWebSecurity
 public class BootAdminApplication {
 
     public static void main(String[] args) {
